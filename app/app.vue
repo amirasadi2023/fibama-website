e47a6eb364641c88ded41d3e489a357e0ed4b47a
Naser <naserrasouli21@gmail.com>
2026-06-17 21:43:13 +0330

    feat: add robots

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,53 @@
     <NuxtPage></NuxtPage>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { landingPageSeo } from "~/mocks/landing";
+
+const siteUrl = "https://fibama.ir";
+const homepageUrl = new URL(landingPageSeo.canonicalPath, siteUrl).toString();
+const ogUrl = new URL(landingPageSeo.openGraph.url, siteUrl).toString();
+const ogImageUrl = new URL(landingPageSeo.openGraph.image, siteUrl).toString();
+const twitterImageUrl = new URL(landingPageSeo.twitter.image, siteUrl).toString();
+
+useSeoMeta({
+  title: landingPageSeo.title,
+  description: landingPageSeo.description,
+  keywords: landingPageSeo.keywords.join(", "),
+  robots: landingPageSeo.robots,
+  themeColor: landingPageSeo.themeColor,
+  ogTitle: landingPageSeo.openGraph.title,
+  ogDescription: landingPageSeo.openGraph.description,
+  ogType: landingPageSeo.openGraph.type,
+  ogLocale: landingPageSeo.openGraph.locale,
+  ogSiteName: landingPageSeo.openGraph.siteName,
+  ogUrl,
+  ogImage: ogImageUrl,
+  ogImageAlt: landingPageSeo.openGraph.imageAlt,
+  twitterCard: landingPageSeo.twitter.card,
+  twitterTitle: landingPageSeo.twitter.title,
+  twitterDescription: landingPageSeo.twitter.description,
+  twitterImage: twitterImageUrl,
+  twitterImageAlt: landingPageSeo.twitter.imageAlt,
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "fa",
+    dir: "rtl",
+  },
+  link: [
+    {
+      rel: "canonical",
+      href: homepageUrl,
+    },
+    {
+      rel: "icon",
+      type: landingPageSeo.favicon.type,
+      sizes: landingPageSeo.favicon.sizes,
+      href: landingPageSeo.favicon.href,
+    },
+  ],
+});
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,93 +13,10 @@
 <script setup lang="ts">
 import { landingPageSeo } from "~/mocks/landing";
 
-useHead({
+useSeoMeta({
   title: landingPageSeo.title,
-  htmlAttrs: {
-    lang: "fa-IR",
-    dir: "rtl",
-  },
-  link: [
-    {
-      rel: "icon",
-      type: landingPageSeo.favicon.type,
-      sizes: landingPageSeo.favicon.sizes,
-      href: landingPageSeo.favicon.href,
-    },
-    {
-      rel: "canonical",
-      href: landingPageSeo.canonicalPath,
-    },
-  ],
-  meta: [
-    {
-      name: "description",
-      content: landingPageSeo.description,
-    },
-    {
-      name: "keywords",
-      content: landingPageSeo.keywords.join(", "),
-    },
-    {
-      name: "robots",
-      content: landingPageSeo.robots,
-    },
-    {
-      name: "theme-color",
-      content: landingPageSeo.themeColor,
-    },
-    {
-      property: "og:title",
-      content: landingPageSeo.openGraph.title,
-    },
-    {
-      property: "og:description",
-      content: landingPageSeo.openGraph.description,
-    },
-    {
-      property: "og:type",
-      content: landingPageSeo.openGraph.type,
-    },
-    {
-      property: "og:locale",
-      content: landingPageSeo.openGraph.locale,
-    },
-    {
-      property: "og:site_name",
-      content: landingPageSeo.openGraph.siteName,
-    },
-    {
-      property: "og:url",
-      content: landingPageSeo.openGraph.url,
-    },
-    {
-      property: "og:image",
-      content: landingPageSeo.openGraph.image,
-    },
-    {
-      property: "og:image:alt",
-      content: landingPageSeo.openGraph.imageAlt,
-    },
-    {
-      name: "twitter:card",
-      content: landingPageSeo.twitter.card,
-    },
-    {
-      name: "twitter:title",
-      content: landingPageSeo.twitter.title,
-    },
-    {
-      name: "twitter:description",
-      content: landingPageSeo.twitter.description,
-    },
-    {
-      name: "twitter:image",
-      content: landingPageSeo.twitter.image,
-    },
-    {
-      name: "twitter:image:alt",
-      content: landingPageSeo.twitter.imageAlt,
-    },
-  ],
+  description: landingPageSeo.description,
+  ogTitle: landingPageSeo.openGraph.title,
+  ogDescription: landingPageSeo.openGraph.description,
 });
 </script>
